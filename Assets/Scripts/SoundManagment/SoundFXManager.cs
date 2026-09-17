826f43eda54a4889caa153a7264e70c226acda26
Matthew Weitzel <matthew.weitzel08@gmail.com>
2026-09-17 18:39:48 -0500

    Sound FX Manager

---
+++ Assets/Scripts/SoundManagment/SoundFXManager.cs
@@ -0,0 +1,46 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class SoundFXManager : MonoBehaviour
+{
+    public static SoundFXManager Instance;
+    [SerializeField] private AudioSource soundFXObject;
+    
+    public float masterAudioVolume = 1f;
+    private void Awake()
+    {
+        Instance ??= this;
+    }
+    
+
+    public void PlaySoundFXClip(AudioClip clip, float volume = 1f)
+    {
+        Transform sourceTransform = Camera.main.gameObject.transform;
+
+        AudioSource audioSource = Instantiate(soundFXObject, sourceTransform.position, quaternion.identity);
+
+        audioSource.clip = clip;
+        audioSource.volume = volume * masterAudioVolume;
+        audioSource.Play();
+
+        float clipLength = audioSource.clip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+      public void PlayRandomSoundFXClip(AudioClip[] clip, float volume)
+    {
+        Transform sourceTransform = Camera.main.gameObject.transform;
+
+        int rand = UnityEngine.Random.Range(0, clip.Length);
+        AudioSource audioSource = Instantiate(soundFXObject, sourceTransform.position, quaternion.identity);
+
+        audioSource.clip = clip[rand];
+        audioSource.volume = volume * masterAudioVolume;
+        audioSource.Play();
+
+        float clipLength = audioSource.clip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+    }
+}
